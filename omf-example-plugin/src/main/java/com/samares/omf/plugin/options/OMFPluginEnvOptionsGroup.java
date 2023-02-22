34c05209c6388a247b2dc102caa8fb9a28b3658f
HugoStinson <hugo.stinson@samares-engineering.com>
2023-02-22 17:56:25 +0100

    Test commit 6 for new repo Jenkins connection

--- omf-example-plugin/src/main/java/com/samares/omf/plugin/options/OMFPluginEnvOptionsGroup.java
+++ omf-example-plugin/src/main/java/com/samares/omf/plugin/options/OMFPluginEnvOptionsGroup.java
@@ -11,7 +11,6 @@ import com.samares.omf.core.ui.environmentoptions.OMFPropertyOptionsGroup;
 
 public class OMFPluginEnvOptionsGroup extends OMFPropertyOptionsGroup {
     public static final String ID = "env.options.omf.plugin";
-
     private static final String OMF_PLUGIN_CATEGORY_NAME = "OMF Plugin";
 
     public OMFPluginEnvOptionsGroup() {
