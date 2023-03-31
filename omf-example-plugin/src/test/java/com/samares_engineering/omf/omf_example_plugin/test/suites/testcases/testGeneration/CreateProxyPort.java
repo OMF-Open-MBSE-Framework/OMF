e5fdfe4b566a5aae859f2d681c46ff09588356e7
Calliope <calliope.dantonlaloy@Samares-Engineering.com>
2023-03-31 17:03:33 +0200

    #OMF-164-Creation_Test_Generation - merge with last dev0
    TO BE DONE:
    - testpackage check ?
    - integrate dev
    - project option

--- omf-example-plugin/src/test/java/com/samares_engineering/omf/omf_example_plugin/test/suites/testcases/testGeneration/CreateProxyPort.java
+++ omf-example-plugin/src/test/java/com/samares_engineering/omf/omf_example_plugin/test/suites/testcases/testGeneration/CreateProxyPort.java
@@ -1,7 +1,10 @@
 package com.samares_engineering.omf.omf_example_plugin.test.suites.testcases.testGeneration;
 
+import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 import com.nomagic.uml2.ext.magicdraw.compositestructures.mdports.Port;
+import com.nomagic.uml2.ext.magicdraw.mdprofiles.Profile;
+import com.nomagic.uml2.ext.magicdraw.mdprofiles.Stereotype;
 import com.samares_engineering.omf.omf_core_framework.utils.OMFUtils;
 import com.samares_engineering.omf.omf_example_plugin.features.testGeneration.utils.OptionsBaseline;
 import com.samares_engineering.omf.omf_test_framework.templates.AbstractTestCase;
@@ -10,11 +13,11 @@ import java.lang.String;
 import java.util.List;
 import java.util.Map;
 
-public class TestPack extends AbstractTestCase {
+public class CreateProxyPort extends AbstractTestCase {
   @Override
   public void initVariables() {
-    testCaseID = "_2021x_2_da1032a_1676023563249_420259_2862"; // http://localhost:9850/refmodel/?ID=_2021x_2_da1032a_1676023563249_420259_2862
-    testPackageName = "TestPack";
+    testCaseID = "_2021x_2_302b0611_1670957645992_118224_3249"; // http://localhost:9850/refmodel/?ID=_2021x_2_302b0611_1670957645992_118224_3249
+    testPackageName = "1 create proxy port";
   }
 
   @Override
@@ -26,13 +29,19 @@ public class TestPack extends AbstractTestCase {
 
   @Override
   public void testAction() {
-    Element owner = findTestedElementByID("_2021x_2_da1032a_1676023576223_970263_2863"); // http://localhost:9850/refmodel/?ID=_2021x_2_da1032a_1676023576223_970263_2863
-    Element diagramElement = findTestedElementByID("_2021x_2_da1032a_1680100753636_268670_2819"); // http://localhost:9850/refmodel/?ID=_2021x_2_da1032a_1680100753636_268670_2819
+    Element owner = findTestedElementByID("_2021x_2_302b0611_1670957597348_883331_3236"); // http://localhost:9850/refmodel/?ID=_2021x_2_302b0611_1670957597348_883331_3236
+    Element diagramElement = findTestedElementByID("_2021x_2_302b0611_1670957709771_148945_3383"); // http://localhost:9850/refmodel/?ID=_2021x_2_302b0611_1670957709771_148945_3383
     if (diagramElement != null) {
-      openDiagram("_2021x_2_da1032a_1680100753636_268670_2819");
+      openDiagram("_2021x_2_302b0611_1670957709771_148945_3383");
     }
 
     Port p1 = OMFUtils.currentProject.getElementsFactory().createPortInstance();
+
+    Profile profile;
+    Stereotype stereotype;
+    profile = StereotypesHelper.getProfile(OMFUtils.currentProject, "SysML");
+    stereotype = StereotypesHelper.getStereotype(OMFUtils.currentProject, "ProxyPort", profile);
+    StereotypesHelper.addStereotype(p1, stereotype);
 
     p1.setOwner(owner);
     p1.setName("p1");
