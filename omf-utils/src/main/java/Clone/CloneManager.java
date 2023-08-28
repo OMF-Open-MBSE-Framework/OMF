d0ef50a8deb792b4b699d582e8b3136f83ba5721
Quentin <Quentin.Cespedes@samares-engineering.com>
2023-08-28 15:34:36 +0200

    AddingCloneManager - Deep clone of Elements including relationships

--- omf-utils/src/main/java/Clone/CloneManager.java
+++ omf-utils/src/main/java/Clone/CloneManager.java
@@ -74,7 +74,7 @@ public class CloneManager {
      * Set the syncElement of the originalElement to its owner, to be able to retrieve it later
      * @param originalElement the original element
      */
-    public void setOriginalElementToClone(Property originalElement) {
+    public void setOriginalElementToClone(Element originalElement) {
         originalElement.setSyncElement(originalElement.getOwner());
     }
 
