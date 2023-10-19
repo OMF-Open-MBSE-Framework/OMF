5ec3499d9be4c004bd4a467e2f38f54800f0b974
HugoStinson <hugo.stinson@samares-engineering.com>
2023-10-19 15:51:22 +0200

    OMF-299 - Test links to files & http links

--- omf-example-plugin/src/main/java/com/samares_engineering/omf/omf_example_plugin/features/sandbox/actions/SandboxUIAction.java
+++ omf-example-plugin/src/main/java/com/samares_engineering/omf/omf_example_plugin/features/sandbox/actions/SandboxUIAction.java
@@ -17,10 +17,12 @@ import java.util.List;
 public class SandboxUIAction extends AUIAction {
     @Override
     public void actionToPerform(List<Element> selectedElements) {
-        UIMessage.err().text("Test error ").link("link to element 1", selectedElements.get(0))
+        UIMessage.info().text("Test error ").link("link to element 1", selectedElements.get(0))
                 .bold("bold").italic("italic").underline("underscore").strike("striketrough")
                 .link("link to element 2", selectedElements.get(1))
                 .linkAction("Custom Action link", () -> UIMessage.info().text("bonus log !").logToConsole())
+                .text("<A href=\"file://C:/Users/HugoStinson/OneDrive - SAMARES ENGINEERING/Bureau/NOTES MENU OMF.txt\"> lien vers mon crashlog> </A>")
+                .text("<A href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"> lien bonus> </A>")
                 .logToConsole();
     }
 
