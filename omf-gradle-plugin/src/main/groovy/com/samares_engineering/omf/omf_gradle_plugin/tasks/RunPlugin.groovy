198eb599d0f8e7835504058c9931dbfd35ed3cdb
HugoStinson <hugo.stinson@samares-engineering.com>
2025-01-10 17:57:38 +0100

    Add missing jvm arguments which where causing failure during magicdraw startup (when giving licence info)

--- omf-gradle-plugin/src/main/groovy/com/samares_engineering/omf/omf_gradle_plugin/tasks/RunPlugin.groovy
+++ omf-gradle-plugin/src/main/groovy/com/samares_engineering/omf/omf_gradle_plugin/tasks/RunPlugin.groovy
@@ -1,9 +1,9 @@
 package com.samares_engineering.omf.omf_gradle_plugin.tasks
 
-import org.gradle.api.GradleException;
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.JavaExec
 import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.jvm.toolchain.JavaToolchainService;
+import org.gradle.jvm.toolchain.JavaToolchainService
 
 abstract class RunPlugin extends JavaExec {
     RunPlugin() {
@@ -11,7 +11,6 @@ abstract class RunPlugin extends JavaExec {
         workingDir "$project.buildDir/install"
         mainClass = 'com.nomagic.magicdraw.Main'
         args 'DEVELOPER', '-verbose'
-        jvmArgs = ["-Xmx16000M", "-Xss1024K", "-Dmd.development=true", "-Dmd.class.path=" + javaVersion]
         javaLauncher.set(
                 project.extensions.getByType(JavaToolchainService).launcherFor {
                     def jvmVersion = project.findProperty('jvmVersion')
@@ -21,5 +20,22 @@ abstract class RunPlugin extends JavaExec {
                     it.languageVersion.set(JavaLanguageVersion.of(jvmVersion))
                 }
         )
+        jvmArgs = [
+                "-XX:+IgnoreUnrecognizedVMOptions",
+                "-Xmx4000M",
+                "-Xss1024K",
+                "-DLOCALCONFIG=true",
+                "-splash:data/splash.png",
+                "-Desi.system.config=data/application.conf",
+                "-Djdk.attach.allowAttachSelf=true",
+                "-Dsun.locale.formatasdefault=true",
+                "-XX:-OmitStackTraceInFastThrow",
+                "--illegal-access=permit",
+                "-Dfile.encoding=UTF-8",
+                "@bin/vm.options",
+                "-Djdk.util.zip.disableZip64ExtraFieldValidation=true",
+                "-Dmd.development=true",
+                "-Dmd.class.path=" + javaVersion
+        ]
     }
 }
--- omf-gradle-plugin/src/main/groovy/com/samares_engineering/omf/omf_gradle_plugin/tasks/RunTests.groovy
+++ omf-gradle-plugin/src/main/groovy/com/samares_engineering/omf/omf_gradle_plugin/tasks/RunTests.groovy
@@ -13,6 +13,30 @@ abstract class RunTests extends JavaExec {
         workingDir "$project.buildDir/install"
         mainClass = 'com.nomagic.magicdraw.commandline.CommandLineActionLauncher'
 
+        javaLauncher.set(
+                project.extensions.getByType(JavaToolchainService).launcherFor {
+                    def jvmVersion = project.findProperty('jvmVersion')
+                    if (jvmVersion == null) {
+                        throw new GradleException("You need to specify the 'jvmVersion' property in your gradle.properties file")
+                    }
+                    it.languageVersion.set(JavaLanguageVersion.of(jvmVersion))
+                }
+        )
+        jvmArgs = [
+                "-XX:+IgnoreUnrecognizedVMOptions",
+                "-Xmx4000M",
+                "-Xss1024K",
+                "-DLOCALCONFIG=true",
+                "-splash:data/splash.png",
+                "-Desi.system.config=data/application.conf",
+                "-Djdk.attach.allowAttachSelf=true",
+                "-Dsun.locale.formatasdefault=true",
+                "-XX:-OmitStackTraceInFastThrow",
+                "--illegal-access=permit",
+                "-Dfile.encoding=UTF-8",
+                "@bin/vm.options",
+                "-Djdk.util.zip.disableZip64ExtraFieldValidation=true"
+        ]
         // Test args
         jvmArgs += [
                 "-Desi.system.config=data/application.conf",
@@ -26,14 +50,5 @@ abstract class RunTests extends JavaExec {
                 "-DuserPwd=$project.properties.twcPassword",
                 "-Dtest=true"
         ]
-        javaLauncher.set(
-                project.extensions.getByType(JavaToolchainService).launcherFor {
-                    def jvmVersion = project.findProperty('jvmVersion')
-                    if (jvmVersion == null) {
-                        throw new GradleException("You need to specify the 'jvmVersion' property in your gradle.properties file")
-                    }
-                    it.languageVersion.set(JavaLanguageVersion.of(jvmVersion))
-                }
-        )
     }
 }
