a91e0194df7a8e2a0a90e89bb78fb66337f20e03
quentin <quentin.cespedes@samares-engineering.com>
2025-03-14 10:45:37 +0100

    Fix 2024x Refresh2 Sysmlv2 compilatiob

--- omf-example-plugin/src/main/java/com/samares_engineering/omf/omf_example_plugin/features/sandbox/actions/CreateCustomTree.kt
+++ omf-example-plugin/src/main/java/com/samares_engineering/omf/omf_example_plugin/features/sandbox/actions/CreateCustomTree.kt
@@ -1,5 +1,13 @@
 package com.samares_engineering.omf.omf_example_plugin.features.sandbox.actions
 
+import com.nomagic.actions.ActionsManager
+import com.nomagic.magicdraw.core.Application
+import com.nomagic.magicdraw.core.Project
+import com.nomagic.magicdraw.ui.browser.Browser
+import com.nomagic.magicdraw.ui.browser.BrowserComponentInfo
+import com.nomagic.magicdraw.ui.browser.BrowserTabTree
+import com.nomagic.magicdraw.ui.browser.BrowserTreeFactory
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element
 import com.samares_engineering.omf.omf_core_framework.feature.registrables.actions.AUIAction
 import com.samares_engineering.omf.omf_core_framework.feature.registrables.actions.annotations.BrowserAction
 import com.samares_engineering.omf.omf_core_framework.feature.registrables.actions.annotations.DiagramAction
@@ -17,29 +25,29 @@ class CreateCustomTree : AUIAction() {
     }
 
     override fun actionToPerform(selectedElements: List<Element>) {
-        val browserTree = BrowserTreeFactory.addBrowserTree(
-            Application.getInstance().mainFrame.browser,
-            "S1",
-            "S2",
-            MyTreeRoot(OMFUtils.getProject()),
-            true,
-            true
-        )
-        browserTree!!.open()
+//        val browserTree = BrowserTreeFactory.addBrowserTree(
+//            Application.getInstance().mainFrame.browser,
+//            "S1",
+//            "S2",
+//            MyTreeRoot(OMFUtils.getProject()),
+//            true,
+//            true
+//        )
+//        browserTree!!.open()
     }
 }
 
+//
+//class MyBrowserTabTree(browser: Browser?, componentInfo: BrowserComponentInfo?, name: String?) : BrowserTabTree(browser, componentInfo, name) {
+//    override fun configureToolbarActions(p0: ActionsManager?) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun configureContextActions(p0: ActionsManager?) {
+//        TODO("Not yet implemented")
+//    }
+//}
 
-class MyBrowserTabTree(browser: Browser?, componentInfo: BrowserComponentInfo?, name: String?) : BrowserTabTree(browser, componentInfo, name) {
-    override fun configureToolbarActions(p0: ActionsManager?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun configureContextActions(p0: ActionsManager?) {
-        TODO("Not yet implemented")
-    }
-}
-
-class MyTreeRoot(project: Project): TreeRoot(project) {
-
-}
+//class MyTreeRoot(project: Project): TreeRoot(project) {
+//
+//}
