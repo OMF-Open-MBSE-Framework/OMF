6706770cdb9ddfcf2f299fcca49193dccf81fd9f
HugoStinson <hugo.stinson@samares-engineering.com>
2025-12-04 15:16:57 +0100

    Remove compilation errors

--- omf-example-plugin/src/main/java/com/samares_engineering/omf/omf_example_plugin/features/sysmlbasic/SysMLBasicFeature.kt
+++ omf-example-plugin/src/main/java/com/samares_engineering/omf/omf_example_plugin/features/sysmlbasic/SysMLBasicFeature.kt
@@ -13,7 +13,6 @@ import com.samares_engineering.omf.omf_core_framework.feature.registrables.livea
 import com.samares_engineering.omf.omf_core_framework.feature.registrables.liveactions.liveaction_engine.LiveActionEngine
 import com.samares_engineering.omf.omf_core_framework.feature.registrables.liveactions.liveaction_engine.LiveActionType
 import com.samares_engineering.omf.omf_core_framework.feature.registrables.options.option.Option
-import com.samares_engineering.omf.omf_example_plugin.features.sysmlbasic.actions.DeactivableAction
 import com.samares_engineering.omf.omf_example_plugin.features.sysmlbasic.live.creation.CreateAutoInterface_OnPortCreation
 import com.samares_engineering.omf.omf_example_plugin.features.sysmlbasic.options.SysMLBasicOptionHelper
 
@@ -28,7 +27,6 @@ class SysMLBasicFeature : SimpleFeature("SysML Basic") {
 
     public override fun initFeatureActions(): List<UIAction> {
         return listOf<UIAction>( //             new ResetListeners()
-            DeactivableAction()
         )
     }
 
